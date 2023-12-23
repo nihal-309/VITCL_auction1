5da19eef412e346dc1e44619ba6c05d90741170d
nihal-309 <nihal1362003@gmail.com>
2023-12-23 09:49:10 +0530

    purse

--- src/App.js
+++ src/App.js
@@ -12,16 +12,16 @@ function App() {
     <div className="App">
       <Header />
       <div className="grid-container">
-        <Card key="1" name="NEPO" purse="400000"/>
-        <Card key="2" name="LASKER" purse="400000"/> 
-        <Card key="3" name="TAL" purse="400000"/>
-        <Card key="4" name="VIDIT" purse="400000"/>
-        <Card key="5" name="MAGNUS" purse="400000"/>
-        <Card key="6" name="CAPABLANCA" purse="400000"/>
-        <Card key="7" name="HIKARU" purse="400000"/>
-        <Card key="8" name="KASPAROV" purse="400000"/>
-        <Card key="9" name="FISCHER" purse="400000"/>
-        <Card key="10" name="KARPOV" purse="400000"/>
+        <Card key="1" name="NEPO" purse="500000"/>
+        <Card key="2" name="LASKER" purse="500000"/> 
+        <Card key="3" name="TAL" purse="500000"/>
+        <Card key="5" name="VIDIT" purse="500000"/>
+        <Card key="5" name="MAGNUS" purse="500000"/>
+        <Card key="6" name="CAPABLANCA" purse="500000"/>
+        <Card key="7" name="HIKARU" purse="500000"/>
+        <Card key="8" name="KASPAROV" purse="500000"/>
+        <Card key="9" name="FISCHER" purse="500000"/>
+        <Card key="10" name="KARPOV" purse="500000"/>
       </div>
       <footer>By Nihal J</footer>
     </div>
