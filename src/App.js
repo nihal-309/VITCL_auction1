3b1100753acc5fc1c9967933c3ac68e071a901b8
nihal-309 <nihal1362003@gmail.com>
2023-12-24 10:52:03 +0530

    Updated purses after day1

--- src/App.js
+++ src/App.js
@@ -12,18 +12,18 @@ function App() {
     <div className="App">
       <Header />
       <div className="grid-container">
-        <Card key="1" name="NEPO" purse="500000"/>
-        <Card key="2" name="LASKER" purse="500000"/> 
-        <Card key="3" name="TAL" purse="500000"/>
-        <Card key="5" name="VIDIT" purse="500000"/>
-        <Card key="5" name="MAGNUS" purse="500000"/>
-        <Card key="6" name="CAPABLANCA" purse="500000"/>
-        <Card key="7" name="HIKARU" purse="500000"/>
-        <Card key="8" name="KASPAROV" purse="500000"/>
-        <Card key="9" name="FISCHER" purse="500000"/>
-        <Card key="10" name="KARPOV" purse="500000"/>
-        <Card key="11" name="HUMPY" purse="500000"/>
-        <Card key="12" name="VISHY" purse="500000"/>
+        <Card key="1" name="NEPO" purse="136000"/>
+        <Card key="2" name="LASKER" purse="133000"/> 
+        <Card key="3" name="TAL" purse="156000"/>
+        <Card key="5" name="VIDIT" purse="108000"/>
+        <Card key="5" name="MAGNUS" purse="140000"/>
+        <Card key="6" name="CAPABLANCA" purse="137000"/>
+        <Card key="7" name="HIKARU" purse="194000"/>
+        <Card key="8" name="KASPAROV" purse="178000"/>
+        <Card key="9" name="FISCHER" purse="134000"/>
+        <Card key="10" name="KARPOV" purse="116000"/>
+        <Card key="11" name="HUMPY" purse="87000"/>
+        <Card key="12" name="VISHY" purse="167000"/>
       </div>
       <footer>By Nihal J</footer>
     </div>
